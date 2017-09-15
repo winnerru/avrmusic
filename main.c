1e9640ccf698bd7a96576f5faa95b2bc8ed8cf22
Victor Chaplinsky <victor.chaplinsky@gmail.com>
2017-09-15 20:12:52 +0300

    Some optimizations

--- main.c
+++ main.c
@@ -11,12 +11,22 @@
 #include "melody1.h"
 
 
+#pragma pack(push, 1)
+union tWord {
+    struct {
+        uint8_t lo;
+        uint8_t hi;
+    };
+    uint16_t data;
+};
+#pragma pack(pop)
+
 const tMelody* melody;
 
 typedef struct {
     uint16_t fNote;
-    uint16_t sPos;
-    uint16_t ePos;
+    tWord sPos;
+    tWord ePos;
 } tPlayerState;
 
 tPlayerState channelState[4];
@@ -30,28 +40,29 @@ ISR(TIMER1_OVF_vect) {
 
     if (melody) {
         for (uint8_t i = 0; i < 4; i++) {
-            uint8_t _s_index = channelState[i].sPos >> 8;
-            uint8_t _e_index = channelState[i].ePos >> 7;
-            if ((channelState[i].sPos += channelState[i].fNote) >= 0x7900) {
-                channelState[i].sPos -= 0x7900;
+            uint8_t _s_index = channelState[i].sPos.hi;
+            uint8_t _e_index = channelState[i].ePos.hi;
+            channelState[i].sPos.data += channelState[i].fNote;
+            if ((channelState[i].sPos.hi) >= 0x79) {
+                channelState[i].sPos.hi -= 0x79;
             }
-            if (++channelState[i].ePos > 16256) {
-                channelState[i].ePos = 16256;
+            if (channelState[i].ePos.hi < 0x7F) {
+                channelState[i].ePos.data++;
             }
-            uint8_t sineVal = pgm_read_byte(&(s_sineTable[_s_index]));
+            int8_t sineVal = pgm_read_byte(&(s_sineTable[_s_index]));
             uint8_t envelopeVal = pgm_read_byte(&(s_envelopeTable[_e_index]));
-            int8_t _sample = (int8_t)((((int16_t)sineVal - 0x80) * envelopeVal) >> 8);
+            int8_t _sample = (int8_t)((((int16_t)sineVal) * envelopeVal) >> 8);
             sample += _sample;
         }
 
-        OCR1AL = (uint8_t)((int16_t)sample + 0x80) >> 1;
+        OCR1AL = (uint8_t)((int16_t)sample + 0x80) >> 2;
 
         if (++_tick >= _tickMax) {
             _tick = 0;
 
             uint8_t channel = pgm_read_byte((uint8_t*)melody);
             uint8_t note = pgm_read_byte((uint8_t*)melody + 1);
-            uint8_t delay = pgm_read_word((uint8_t*)melody + 2);
+            uint8_t delay = pgm_read_byte((uint8_t*)melody + 2);
 
             if ( 0 == delay ) {
                 OCR1AL = 0x80;
@@ -59,8 +70,8 @@ ISR(TIMER1_OVF_vect) {
                 melody = NULL;
             } else {
                 _tickMax = delay << 6;
-                channelState[channel].ePos = 0;
-                channelState[channel].sPos = 0;
+                channelState[channel].ePos.data = 0;
+                channelState[channel].sPos.data = 0;
                 channelState[channel].fNote = pgm_read_word(&(notes[note]));
                 melody++;
             }
